6f92d6f9e8b50163c4d4b6a4095121bc787fd632
peacess <peacess@163.com>
2023-12-17 09:59:34 +0800

    async 5 nanos/call

---
+++ src/async_thread/performe/thread_async.rs
@@ -0,0 +1,21 @@
+use tokio::runtime::Handle;
+
+fn main() {
+    const MAX: u64 = 1000_000_000;
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let start = std::time::Instant::now();
+        let mut sum = 0;
+        for i in 0..MAX {
+            let t = add(i).await;
+            sum += t;
+        }
+        let du = start.elapsed();
+        println!("sum: {} \n all: {}, t/s: {}", sum, du.as_nanos(),du.as_nanos()/MAX as u128);
+    });
+}
+
+#[inline(never)]
+async  fn add(c: u64) -> u64 {
+    return c + 1;
+}
---
+++ src/async_thread/performe/thread_fn.rs
@@ -0,0 +1,21 @@
+
+fn main() {
+    const MAX: u64 = 1000_000_000;
+    let mut t = std::thread::spawn(||{
+        let start = std::time::Instant::now();
+        let mut sum = 0;
+        for i in 0..MAX {
+            let t = add(i);
+            sum += t;
+        }
+        let du = start.elapsed();
+        println!("sum: {} \n all: {}, t/s: {}", sum, du.as_nanos(),du.as_nanos()/MAX as u128);
+
+    });
+    let _ = t.join();
+}
+
+#[inline(never)]
+fn add(c: u64) -> u64 {
+    return c + 1;
+}
